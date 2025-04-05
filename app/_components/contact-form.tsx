02969ce689884254afa23aef0af4d604eaf28043
Andrew-Marchment <a.marchment25@gmail.com>
2025-04-05 11:19:17 -0400

    react-hook-form integration for contact form

--- app/_components/contact-form.tsx
+++ app/_components/contact-form.tsx
@@ -1,8 +1,32 @@
 import SubmitButton from "./submit-button";
+import { useForm } from "react-hook-form";
+import { EMAIL_REGEX } from "../utils/constants";
+
+type contactFormTypes = {
+  name: string;
+  email: string;
+  message: string;
+};
 
 export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  function onSubmit(data: contactFormTypes) {
+    console.log(data);
+  }
+
   return (
-    <form className="contact-form">
+    <form className="contact-form" onSubmit={handleSubmit(onSubmit)}>
       <label className="form__label" htmlFor="name">
         Name
       </label>
@@ -10,8 +34,10 @@ export default function ContactForm() {
         className="form__input"
         id="name"
         type="text"
+        {...register("name", { required: "Please enter your name" })}
         placeholder="Enter your name..."
       />
+      <p className="form__error">{errors.name?.message}</p>
       <label className="form__label" htmlFor="email">
         Email
       </label>
@@ -19,17 +45,27 @@ export default function ContactForm() {
         className="form__input"
         id="email"
         type="email"
+        {...register("email", {
+          required: "Please enter your email",
+          pattern: {
+            value: EMAIL_REGEX,
+            message: "Please enter a valid email address",
+          },
+        })}
         placeholder="Enter your email..."
       />
+      <p className="form__error">{errors.email?.message}</p>
       <label className="form__label" htmlFor="message">
         Message
       </label>
       <textarea
         className="form__input"
         id="message"
+        {...register("message", { required: "Please include a message" })}
         placeholder="Enter your message..."
         rows={8}
       />
+      <p className="form__error">{errors.message?.message}</p>
       <SubmitButton>Send Message</SubmitButton>
     </form>
   );
